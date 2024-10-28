04f848601f42f7f2c52cf87d1a4f4a68d60324dd
Steinwandter Oliver <oliver.steinwandter@oebb.at>
2024-10-28 09:02:55 +0100

    fixes uuid in api

--- api/document/index.js
+++ api/document/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
     context.res = {
       // status: 200, /* Defaults to 200 */
-      body: [{ id: crypto.randomUUID(), content: "hallo", title: 'api doc', lastModifiedUtc: new Date().toISOString() }],
+      body: [{ id: "9a96c9bf-8f6d-4deb-8a4e-1de8e7e33553", content: "hallo", title: 'api doc', lastModifiedUtc: new Date().toISOString() }],
     };
   };
