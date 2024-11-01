435cf221be8405fc9b60ec36dce5e153f2904aee
Steinwandter Oliver <oliver.steinwandter@oebb.at>
2024-11-01 10:14:40 +0100

    Sort list by createdUtc desc

--- src/components/DocumentList.jsx
+++ src/components/DocumentList.jsx
@@ -6,24 +6,27 @@ import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 const DocumentList = ({ documents, onOpenDocument, onDeleteDocument }) => {
     return (
         <ul className="list">
-            {documents.sort((a, b) => a.title.localeCompare(b.title)).map(document => (
-                <li key={document.id} className="list-item" onClick={() => onOpenDocument(document.id)}>
-                    <span>{format(new Date(document.createdUtc), 'd.MM.yyyy, H:mm:ss')}</span>
-                    <span>{document.title}</span>
-                    <div>
-                        <IconButton
-                        className="delete-button"
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                onDeleteDocument(document);
-                            }}
-                            color='error'
-                        >
-                            <DeleteForeverIcon/>
-                        </IconButton>
-                    </div>
-                </li>
-            ))}
+            {documents
+                .sort((a, b) => new Date(b.createdUtc) - new Date(a.createdUtc))
+                .map(document => (
+                    <li key={document.id} className="list-item" onClick={() => onOpenDocument(document.id)}>
+                        <span>{format(new Date(document.createdUtc), 'd.MM.yyyy, H:mm:ss')}</span>
+                        <span>{document.title}</span>
+                        <div>
+                            <IconButton
+                                className="delete-button"
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    onDeleteDocument(document);
+                                }}
+                                color='error'
+                            >
+                                <DeleteForeverIcon />
+                            </IconButton>
+                        </div>
+                    </li>
+                ))
+            }
         </ul>
     );
 };
